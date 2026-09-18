17407bec39f371b3fe5853e75d22a2b0a2d108b6
econdrey1 <elijahcondrey364@gmail.com>
2026-09-18 19:33:07 -0400

    Add files via upload

---
+++ task.py
@@ -0,0 +1,13 @@
+print("Bmore Fast")
+
+
+city = input("what city did you grow up in? ")
+
+
+print(city)
+
+pet = input("what is the name of your pet? ")
+print(pet)
+
+result = city + " " + pet
+print(result)
